62edd8ff21f2f4a5fbec202b4bcc90c87229c720
Sijan666 <666majharulislam@gmail.com>
2026-02-20 23:58:31 +0600

    feat : complete header with responsive for xs

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,10 @@
+
+import { GrShop } from "react-icons/gr"
 import Container from "../Container"
 import Flex from "../Flex"
 import Images from "../Images"
 import logo from '/logo.png'
+import { LiaUser } from "react-icons/lia"
 
 
 const Header = () => {
@@ -9,8 +12,8 @@ const Header = () => {
         <>
         <header className="header py-8">
             <Container>
-                <Flex>
-                    <Flex className={'gap-x-14'}>
+                <Flex className={'lg:justify-between flex flex-col lg:flex-row gap-y-8'}>
+                    <Flex className={'lg:gap-x-14 flex flex-col lg:flex-row gap-y-8'}>
                         <div className="logo">
                             <Images imgSrc={logo}/>
                         </div>
@@ -24,6 +27,14 @@ const Header = () => {
                             </ul>
                         </nav>
                     </Flex>
+                    <div className="socialIcons flex gap-x-2 items-center">
+                        <div className="p-3 bg-[#F0F6FD] rounded-full text-[#0F73F6] hover:bg-[#0F73F6] duration-300 hover:text-white cursor-pointer">
+                            <LiaUser size={20}/>
+                        </div>
+                        <div className="p-3 bg-[#F0F6FD] rounded-full text-[#0F73F6] hover:bg-[#0F73F6] duration-300 hover:text-white cursor-pointer">
+                            <GrShop  size={20}/>
+                        </div>
+                    </div>
                 </Flex>
             </Container>
         </header>
