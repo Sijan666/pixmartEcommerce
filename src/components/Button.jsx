6b0f9ca1d461b48b692864f73c31b2f10802d2fd
Sijan666 <666majharulislam@gmail.com>
2026-02-21 02:21:59 +0600

    feat : complete banner part with responsive for xs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 
 const Button = ({btnText , className}) => {
     return (
-        <button className={`cursor-pointer duration-500 ${className}`}>{btnText}</button>
+        <button className={`cursor-pointer duration-500 bg-[#0F73F6] text-white hover:bg-white hover:text-[#0F73F6] font-bold rounded-[10px] border border-[#0F73F6] ${className}`}>{btnText}</button>
     )
 }
 
--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,10 +1,29 @@
-
+import Button from "../Button"
+import Container from "../Container"
+import Images from "../Images"
+import banneImg from '../../assets/bannerImg.png'
 
 
 const Banner = () => {
     return (
         <>
-        <h1>Banner</h1>
+        <section className="banner py-20 bg-new">
+            <Container className={'text-center px-2'}>
+                <h1 className="lg:text-[60px] text-[30px] font-bold lg:px-60 text-black pb-5">Unleash Your Creativity to Utmost <span className="text-[#0F73F6]">Potential</span> with Us</h1>
+                <p className="lg:text-[20px] text-base text-[#222222]/80">Discover the building blocks of your next masterpiece: premium digital assets at your fingertips.</p>
+                {/* input */}
+                <div className="pt-8 flex gap-x-3 gap-y-3 lg:justify-center justify-normal flex-col lg:flex-row pb-20">
+                    <input type="text" placeholder="Find fonts, templates, illustrations, and more…" className="px-8 py-6 bg-white rounded-[10px] outline-none lg:w-[436px] " />
+                    <Button btnText={'Search Now'} className={'lg:px-[46px] px-8 lg:py-6 py-4 w-fit mx-auto lg:mx-0'}/>
+                </div>
+                {/* input */}
+                {/* img */}
+                <div className="px-2">
+                    <Images imgSrc={banneImg} className={'w-full'}/>
+                </div>
+                {/* img */}
+            </Container>
+        </section>
         </>
     )
 }
